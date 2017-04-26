9ec8f73dba2c41353df7944c649424b74a2c49f5
adley <adley.phu@gmail.com>
2017-04-27 02:08:18 +1000

    WIP

--- kern/syscall/file.c
+++ kern/syscall/file.c
@@ -1,9 +1,9 @@
 #include <types.h>
 #include <kern/errno.h>
 #include <kern/fcntl.h>
-#include <kern/limits.h>
 #include <kern/stat.h>
 #include <kern/seek.h>
+#include <limits.h>
 #include <lib.h>
 #include <uio.h>
 #include <thread.h>
@@ -21,11 +21,35 @@
  */
 
 int sys_open(userptr_t filename, int flags, userptr_t *ret) {
-	(void)filename;
-	(void)flags;
-	(void)ret;
+	char *path = (char *)filename;
+	struct vnode *v;
+	int result = vfs_open(path, flags, 0, &v);
+	if (result) {
+		return result;
+	}
 
-	curproc->descriptor_table[0] = NULL;
+	for (int i = 0; i < OPEN_MAX; i++ ) {
+		if (curproc->descriptor_table[i] == NULL) {
+			break;
+		}
+	}
+	if (i == OPEN_MAX) {
+		return EMFILE;
+	}
+
+	struct open_file *file = kmalloc(sizeof(struct open_file*));
+	char buf[128];
+	struct iovec iov;
+	struct uio myuio;
+ 
+	uio_kinit(&iov, &myuio, buf, sizeof(buf), 0, UIO_READ);
+	result = VOP_READ(vn, &myuio);
+
+	file->v_ptr = v;
+	file->f_ptr = v->vn_ops->vop_read(v, file_pointer
+
+	curproc->descriptor_table[i] = file;
+	*ret = i;
 	
 	return 0;
 }
@@ -33,7 +57,7 @@ int sys_open(userptr_t filename, int flags, userptr_t *ret) {
 int sys_close(int file) {
 	(void)file;
 	panic("delete me");
-
+	proc_create(
 	return 0;
 }
 
