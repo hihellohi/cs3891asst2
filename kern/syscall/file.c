b1378ef1c0e800c11ec0883c733a022c5a5ce7c1
Kevin <hihellohi610@gmail.com>
2017-04-27 02:08:26 +1000

    dup2 plus misc bug fix

--- kern/syscall/file.c
+++ kern/syscall/file.c
@@ -20,7 +20,7 @@
  * Add your file-related functions here ...
  */
 
-int sys_open(userptr_t filename, int flags, userptr_t *ret) {
+int sys_open(userptr_t filename, int flags, int *ret) {
 	(void)filename;
 	(void)flags;
 	(void)ret;
@@ -31,8 +31,29 @@ int sys_open(userptr_t filename, int flags, userptr_t *ret) {
 }
 
 int sys_close(int file) {
-	(void)file;
-	panic("delete me");
+	if(file < 0 || file >= OPEN_MAX || !curproc->descriptor_table[file]) {
+		return EBADF;
+	}
+
+	vfs_close(curproc->descriptor_table[file]->v_ptr);
+
+	curproc->descriptor_table[file] = NULL;
+
+	return 0;
+}
+
+int sys_dup2(int oldfd, int newfd) {
+	if(oldfd < 0 || oldfd >= OPEN_MAX ||
+			newfd < 0 || newfd >= OPEN_MAX ||
+			!curproc->descriptor_table[oldfd]) {
+		return EBADF;
+	}
+
+	if(curproc->descriptor_table[newfd]){
+		sys_close(newfd);
+	}
+
+	curproc->descriptor_table[newfd] = curproc->descriptor_table[oldfd];
 
 	return 0;
 }
